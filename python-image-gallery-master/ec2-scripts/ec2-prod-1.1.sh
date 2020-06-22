e1dc4f1a048a74c308ffddfed9c1cad7785b9113
Roman Owensby <rco0009@auburn.edu>
2020-06-21 20:55:48 -0500

    first commit

--- python-image-gallery-master/ec2-scripts/ec2-prod-1.1.sh
+++ python-image-gallery-master/ec2-scripts/ec2-prod-1.1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 export IMAGE_GALLERY_SCRIPT_VERSION="1.1"
-CONFIG_BUCKET="edu.au.cc.image-gallery-config"
+CONFIG_BUCKET="edu.au.cc.image-gallery-owensby-config"
 
 # Install packages
 yum -y update
@@ -10,7 +10,7 @@ amazon-linux-extras install -y nginx1
 
 # Configure/install custom software
 cd /home/ec2-user
-git clone https://github.com/cdavidshaffer/python-image-gallery.git
+git clone hhttps://github.com/rco0009/python-image-gallery.git
 chown -R ec2-user:ec2-user python-image-gallery
 su ec2-user -l -c "cd ~/python-image-gallery && pip3 install -r requirements.txt --user"
 
